196cb8c46b3de3e15c00808ab7910401d40d9540
jaRue1 <jaruejohnson@outlook.com>
2021-04-23 12:22:18 -0400

    created firebase project structure

---
+++ functions/src/store/index.js
@@ -0,0 +1,12 @@
+var admin = require("firebase-admin")
+
+let serviceAccount = require("../../credentials.json")
+let db;
+function reconnectToFirestore() {
+  if (!db) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    })
+    db = admin.firestore()
+  }
+}
